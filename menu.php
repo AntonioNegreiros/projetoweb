248428b2ea0dbe3a0993847f2bf0b4e613b746ff
Antônio Negreiros <antonio.negreiros01@gmail.com>
2024-08-30 21:23:14 -0300

    Inicial

---
+++ menu.php
@@ -0,0 +1,7 @@
+<nav>
+    <ul class="menu">
+    <li><a href="index.php">Inicial</a></li>
+    <li><a href="produtos.php">Produtos</a></li>
+    <li><a href="contato.php">Contato</a></li>
+</ul>
+</nav>
